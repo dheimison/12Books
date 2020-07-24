4351e260b984853a9699fe66508fcb851b674c37
Dheimison Siqueira <dheimisonlo@gmail.com>
2020-07-24 00:14:48 -0300

    chore: adds initial web app settings

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier', 'prettier/react'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+    'import/no-unresolved': 'off',
+    'no-param-reassign': 'off',
+    'import/prefer-default-export': 'off',
+    'import/extensions': 'off',
+  },
+};
