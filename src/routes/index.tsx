c17f78dc96f83ccd5315bedecc3fac14cdf33267
Dheimison Siqueira <dheimisonlo@gmail.com>
2020-07-26 22:41:50 -0300

    chore: imports the book's details page and adds to the routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import HomePage from 'pages/HomePage';
+import BookDetail from 'pages/BookDetail';
 
 export default function Routes() {
   return (
     <Switch>
+      <Route path="/Book" exact component={BookDetail} />
       <Route path="/" component={HomePage} />
     </Switch>
   );
