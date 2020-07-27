442a95ee206a480b84cd09dda64d14c50a98661c
Dheimison Siqueira <dheimisonlo@gmail.com>
2020-07-27 00:54:58 -0300

    Updates

---
+++ precache-manifest.e5104fd555f3c60366873907d930bee9.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35fc3a0d36879e794e83129ce5298bc7",
+    "url": "/12Books/index.html"
+  },
+  {
+    "revision": "fe28aa848df634527f27",
+    "url": "/12Books/static/js/2.a3d528e4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/12Books/static/js/2.a3d528e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4083a8d5fcbfc2fc558f",
+    "url": "/12Books/static/js/main.54cc00e4.chunk.js"
+  },
+  {
+    "revision": "2782092b5d2424e5163f",
+    "url": "/12Books/static/js/runtime-main.c2b2e125.js"
+  },
+  {
+    "revision": "58fe6873cccf02925996d7df87f94cf7",
+    "url": "/12Books/static/media/1984-George-Orwell.58fe6873.jpg"
+  },
+  {
+    "revision": "380c6cad63a384d3265b82a631cc8f2c",
+    "url": "/12Books/static/media/Count-Zero.380c6cad.jpg"
+  },
+  {
+    "revision": "96cd67e37b6e37f321ded08e5745cfd4",
+    "url": "/12Books/static/media/Do-Androids-Dream-of-Electric-Sheep.96cd67e3.jpg"
+  },
+  {
+    "revision": "7edb0096e1ddd979f8f09320d04745e9",
+    "url": "/12Books/static/media/Infomocracy.7edb0096.jpg"
+  },
+  {
+    "revision": "9e0e8c843737ef29989423c5ee05479d",
+    "url": "/12Books/static/media/Life- the-Universe-and-Everything.9e0e8c84.jpg"
+  },
+  {
+    "revision": "6a93bedc8ec2fd2fb1faaa31f370b210",
+    "url": "/12Books/static/media/Monalisa-Overdrive.6a93bedc.jpg"
+  },
+  {
+    "revision": "1009baaea2971d6605cb17a6b57e9d6f",
+    "url": "/12Books/static/media/Neuromancer.1009baae.jpg"
+  },
+  {
+    "revision": "337ed000d3c400a41aa4d80d605fd721",
+    "url": "/12Books/static/media/Null-States-Book-Two-of-the-Centenal-Cycle.337ed000.jpg"
+  },
+  {
+    "revision": "3f87106ae0d216e61fb864b68f62403b",
+    "url": "/12Books/static/media/Snow-Crash.3f87106a.jpg"
+  },
+  {
+    "revision": "ba3f9a7c07e0ceb22fd1ec0f288874c2",
+    "url": "/12Books/static/media/So-Long-and-Thanks-For-All-the-Fish.ba3f9a7c.jpg"
+  },
+  {
+    "revision": "33e1508d103b51a36a16d078c4eddf1f",
+    "url": "/12Books/static/media/The-Hitchhikers-Guide-to-the-Galaxy.33e1508d.jpg"
+  },
+  {
+    "revision": "e597d280d3739fc0f623d951e981129c",
+    "url": "/12Books/static/media/The-Restaurant-at-the-End-of-the-Universe.e597d280.jpg"
+  }
+]);
